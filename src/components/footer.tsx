8903f19464d6a872e6ea6de4315e4c4f9014f224
Alex Pierini <alex_pierini@hotmail.com>
2024-02-29 00:19:11 -0500

    Footer layout

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import { Box, Container, Typography } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Box bgcolor='primary.main'>
+      <Typography variant="body2" align="center" mr={2}>
+        © Alex Pierini - 02/2024
+      </Typography>
+    </Box>
+  )
+}
