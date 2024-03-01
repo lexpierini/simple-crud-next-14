d461e1312e72e185784159b8b8aed1bf4fb36936
Alex Pierini <alex_pierini@hotmail.com>
2024-03-01 00:14:53 -0500

    Fix footer text color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { Box, Container, Typography } from "@mui/material";
 
 export default function Footer() {
   return (
-    <Box bgcolor='primary.main'>
+    <Box bgcolor='primary.main' color='white'>
       <Typography variant="body2" align="center" mr={2}>
         © Alex Pierini - 02/2024
       </Typography>
